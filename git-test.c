b6764a6dbe05171ea6845c2ca648e44bcd9a7dd5
Denzy7 <dennismyner7@gmail.com>
2021-09-25 16:32:50 +0300

    Added a file saving function

--- git-test.c
+++ git-test.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(int argc, char** argv)
 {
 	printf("Started at %s\n",argv[0]);
@@ -15,7 +15,16 @@ int main(int argc, char** argv)
 	int input_int = 0;
 	scanf("%d",&input_int);
 	printf("%d+%d=%d\n",input_int,10,(input_int+10));
-	
+
+	//New feature for v0.3
+	printf("Please enter where to save your file: (25 char. max)\n");
+	char file_name[26];
+	scanf("%s",file_name);
+	FILE* f = fopen(file_name,"wb");
+
+	fwrite(input, 1, strlen(input),f);
+	fclose(f);
+
 	//This is modified stuff for git
 	//1234456
 
